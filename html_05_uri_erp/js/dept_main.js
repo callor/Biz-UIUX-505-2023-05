ec8854872774d814feca22956c31d06f111f81d8
callor <callor@callor.com>
2023-06-09 11:53:59 +0900

    html

---
+++ html_05_uri_erp/js/dept_main.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+  /*
+     dept_main 에서 입력하는 요소중에서
+     거래처코드, 거래처명, 대표전화, 담당자명, 담당자연락처는
+     반드시 입력해야 하는 요소이다
+
+     각 요소를 입력하지 않고 저장을 할경우
+     alert 경고를 띄우고, 값을 입력하도록 하는 
+     front validation(유효성 검사)를 실행하시오
+     */
+});
